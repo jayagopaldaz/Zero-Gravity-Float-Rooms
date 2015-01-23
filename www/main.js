9c7c0db04b66acbd45a5aca540f871cd85390e35
jayagopaldaz <jayagopaldaz@gmail.com>
2015-01-22 23:20:21 -0600

    test for demo
    test.

--- www/main.js
+++ www/main.js
@@ -1,5 +1,5 @@
 ﻿//var ipAndPort='http://192.168.0.11:8081';
-var ipAndPort='http://72.182.7.206:8081';
+var ipAndPort='http://72.182.78.244:8081';
 var pause=false;
 var loaded=0;
 var winW,winH;
@@ -23,7 +23,7 @@ setInterval(function(){
     s$=s<10 ? "0"+s : s; 
     if(m+s>0) timeN=m+":"+s$
     else timeN=false;
-    tank[t].setData(timeN,tempN.toPrecision(3)+"°F");
+    tank[t].setData(timeN,"12:04 PM"); //tempN.toPrecision(3)+"°F");
   }
   if(insideTank!=-1){
     if(!tank[insideTank].time) tankTimes.setText("NOT IN USE");
